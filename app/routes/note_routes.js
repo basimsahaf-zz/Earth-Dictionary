888d6bd64a6aa4deaa753dd4a6b64c81f2d1462e
basimsahaf <bssahaf@uwaterloo.ca>
2017-04-29 04:22:25 -0400

    Added entry status, fixed html query

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,12 +1,13 @@
 // routes/note_routes.js
 
 var ObjectID = require('mongodb').ObjectID;
+var query = "Stratosphere" //raw HTML request
 
 module.exports = function(app, db) {
 
   app.get('/notes/:id', (req, res) => {
     const id = req.params.id;
-    const details = { key: "Stratosphere" };
+    const details = { key: query };
     db.collection('notes').find(details).toArray(function(err, results){
     console.log(results); // output all records
     res.send(results);
@@ -16,7 +17,7 @@ module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     const note = { val: req.body.body, key: req.body.title,
                    likes: req.body.likes, cont: req.body.cont,
-                   cat: req.body.cat};
+                   cat: req.body.cat, status: req.body.stat};
     db.collection('notes').insert(note, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
@@ -43,7 +44,7 @@ module.exports = function(app, db) {
     const details = { '_id': new ObjectID(id) };
     const note = { val: req.body.body, key: req.body.title,
                    likes: req.body.likes, cont: req.body.cont,
-                   cat: req.body.cat};
+                   cat: req.body.cat, status: req.body.stat};
     db.collection('notes').update(details, note, (err, result) => {
       if (err) {
           res.send({'error':'An error has occurred'});
