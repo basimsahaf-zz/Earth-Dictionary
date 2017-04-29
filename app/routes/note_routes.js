c64058de76b8f41641b43f6a52fceeeadda59965
basimsahaf <bssahaf@uwaterloo.ca>
2017-04-29 05:01:07 -0400

    Fixed push requests, status handling

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,23 +1,29 @@
 // routes/note_routes.js
 
 var ObjectID = require('mongodb').ObjectID;
-var query = "Stratosphere" //raw HTML request
 
 module.exports = function(app, db) {
 
   app.get('/notes/:id', (req, res) => {
     const id = req.params.id;
-    const details = { key: query };
+    const details = { key: id };
     db.collection('notes').find(details).toArray(function(err, results){
-    console.log(results); // output all records
-    res.send(results);
+      var ret = [];
+    for(var i =0;i<results.length;i++) {
+      console.log(results[i]);
+      if(results[i]["status"] == 1) {
+        ret.push(results[i]);
+      }
+    }
+    console.log(ret); // output all records
+    res.send(ret);
    });
   });
 
   app.post('/notes', (req, res) => {
-    const note = { val: req.body.body, key: req.body.title,
+    const note = { key:req.body.key, val:req.body.val,
                    likes: req.body.likes, cont: req.body.cont,
-                   cat: req.body.cat, status: req.body.stat};
+                   cat: req.body.cat, status: 0};
     db.collection('notes').insert(note, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
@@ -29,7 +35,7 @@ module.exports = function(app, db) {
 
   app.delete('/notes/:id', (req, res) => {
     const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
+    const details = { key: query };
     db.collection('notes').remove(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
@@ -41,8 +47,8 @@ module.exports = function(app, db) {
 
   app.put('/notes/:id', (req, res) => {
     const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    const note = { val: req.body.body, key: req.body.title,
+    const details = { key: id };
+    const note = { key:req.body.key, val:req.body.val,
                    likes: req.body.likes, cont: req.body.cont,
                    cat: req.body.cat, status: req.body.stat};
     db.collection('notes').update(details, note, (err, result) => {
