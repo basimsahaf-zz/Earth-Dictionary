5c3f8bd36f02ad9869ac1be7571c5db99e4f03e4
Brijendra Nag <brijendra98@gmail.com>
2017-04-29 19:19:32 -0400

    made function to get all endpoints

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -18,6 +18,22 @@ module.exports = function(app, db) {
    });
   });
 
+  // Function for GET /notes requests to retrieve
+  // all stored data from existing API endpoints
+  app.get('/notes', (req, res) => {
+
+    const details = {status: 1};
+    db.collection('notes').find(details).toArray(function(err, results){
+      var ret = [];
+    for(var i =0;i<results.length;i++) {
+      if(results[i]["status"] == 1) {
+        ret.push(results[i]);
+      }
+    }
+    res.send(ret);
+   });
+  });
+
   // Function for POST /notes to create a new API endpoint
   app.post('/notes', (req, res) => {
     const note = { key:req.body.key, val:req.body.val,
